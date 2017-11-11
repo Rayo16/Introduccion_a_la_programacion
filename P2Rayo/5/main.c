e628156c65080c567f82018c82d6b3d1e22c2ad3
Rayo16 <rodrix.g@hotmail.com>
2017-11-11 01:04:22 -0600

    h
    m

--- P2Rayo/5/main.c
+++ P2Rayo/5/main.c
@@ -4,28 +4,35 @@
 int main(int argc, char *argv[]) {
 	system("color 1f");
 	int a,b,c;
-	char var='0';
+	char var=0;
 	printf("Programa que recibe dos enteros y un cáracter para hacer cálculos\n+ para sumar, - para restarlos, * para multiplicarlos y /para dividirlos y S o s para salir.");
 	printf("\nIngrese el primer numero: ");
-	scanf("%i",&a);
+	scanf("%i",& a);
 	printf("\nIngrese el segundo numero: ");
 	scanf("%i",&b);
 	printf("\nIngrese el operador: ");
-	scanf("%c",& var);
-	while (var!='S' || var!='s'){
-		if(var=='+'){
-		c=a+b;}
+	scanf("%s",& var);
+	do{
+		if(var == '+'){
+		c=a+b;
+		printf("\nEl resultado de su operacion es: %i",c);}
 		else if(var=='-'){
-		c=a-b;}
+		c=a-b;
+		printf("\nEl resultado de su operacion es: %i",c);}
 		else if(var=='*'){
-		c=a*b;}
+		c=a*b;
+		printf("\nEl resultado de su operacion es: %i",c);}
 		else if(var=='/'){
-		c=a/b;}
+		c=a/b;
+		printf("\nEl resultado de su operacion es: %i",c);}
+		else{
+				printf("\nOperacion no valida");
+		}
 		
-		printf("\nEl resultado de su operación es: %i",c);
-		printf("\nSi desea salir presione S o s");
-		scanf("%c",&var);
-	}
+		
+		printf("\nSi desea salir presione S o s: ");
+		scanf("%s",&var);
+	}while (var!='S' || var!='s');
 	getch();
 	return 0;
 }
